96f16ee07c70a106b6aed113f4ad35a63a282329
Vânia Lopes Santana <vhlsantana@gmail.com>
2021-05-07 00:01:17 +0100

    created a small tutorial to study the python primitive data types and how to set them.

---
+++ tutorial_small.py
@@ -0,0 +1,35 @@
+# this is a comment
+
+#### VARIABLES ####
+# type of primitive variables in python:
+# string --> one or more characters: "vania"
+# integer --> decimal numbers: [0-9]
+# float --> non decimal numbers: [0.1-9.9]
+# boolean --> True or False/ 1 or 0
+ 
+# how to set variables: 
+#
+
+name = "Ritchy"
+print(name)
+print(type(name))
+
+age = 10
+print(age)
+print(type(age))
+
+age = 0.20
+print(age)
+print(type(age))
+
+is_male = True 
+print(is_male)
+print(type(is_male))
+
+is_female = False
+print(is_female)
+print(type(is_female))
+
+is_open = "1"
+print(is_open)
+print(type(is_open))
